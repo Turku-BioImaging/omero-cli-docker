aad33b049f63e1c6e9d7321e049d88e423cbc854
Junel Solis <junel.solis@gmail.com>
2022-05-19 12:52:39 +0200

    fix handling of secure password

--- copy.ps1
+++ copy.ps1
@@ -2,13 +2,14 @@ Write-Output "-----------------------------"
 Write-Output "#### OMERO Upload Script ####"
 Write-Output "-----------------------------"
 
-$server = Read-Host -Prompt "Server address[omero.utu.fi]"
+$server = Read-Host -Prompt "Server address[omero.cci.sahlgrenska.gu.lcl]"
 $port = Read-Host -Prompt "Port[4064]"
 $username = Read-Host -Prompt "Username"
 $password = Read-Host -Prompt "Password" -AsSecureString
+$clearTextPassword = ConvertFrom-SecureString -SecureString $password -AsPlainText
 
 if ([string]::IsNullOrWhiteSpace($server)) {
-    $server = "omero.utu.fi"
+    $server = "omero.cci.sahlgrenska.gu.lcl"
 }
 
 if ([string]::IsNullOrWhiteSpace($port)) {
@@ -16,6 +17,4 @@ if ([string]::IsNullOrWhiteSpace($port)) {
 }
 
 docker pull ghcr.io/turku-bioimaging/omero-cli-docker:0.1.0
-docker run --mount "type=bind,source=$(Get-Location)/to_upload,target=/upload" -it ghcr.io/turku-bioimaging/omero-cli-docker:0.1.0 /bin/bash -c "omero login $username@${server}:${port} -w ${password}; omero import /upload"
-
-# docker run -it  omero-cli-docker omero group list
+docker run --mount "type=bind,source=$(Get-Location)/to_upload,target=/upload" -it ghcr.io/turku-bioimaging/omero-cli-docker:0.1.0 /bin/bash -c "omero login $username@${server}:${port} -w ${clearTextPassword}; omero import /upload"
